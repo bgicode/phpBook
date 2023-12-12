fe8a11ce29db25c4abc8077433af348214b75efc
zahernaher <74210661+zahernaher@users.noreply.github.com>
2023-12-12 12:11:37 +0300

    logic values php8 change

--- Part_02_PHP_basics/07_chapter_expr_oper_PHP/comparison operations/bool.php
+++ Part_02_PHP_basics/07_chapter_expr_oper_PHP/comparison operations/bool.php
@@ -2,4 +2,6 @@
     $hundred = 100;
     if ($hundred == 1) echo "хм, странно... переменная равна 1!<br />";
     if ($hundred == true) echo "переменная истинна!<br />";
+    if ("" == 0) echo "совпадение!";//в PHP 8 возврвщает false https://www.php.net/manual/ru/migration80.incompatible.php
+    if ("Universe" == 0) echo "совпадение!";//в PHP 8 возврвщает false
 ?>
