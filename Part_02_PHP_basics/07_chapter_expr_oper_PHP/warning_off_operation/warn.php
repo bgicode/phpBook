a317b4a0f8817c3e70a47f4cd23d2df1de53701b
zahernaher <74210661+zahernaher@users.noreply.github.com>
2023-12-12 13:38:45 +0300

    warnoff.php and warn.php php8 fix

--- Part_02_PHP_basics/07_chapter_expr_oper_PHP/warning_off_operation/warn.php
+++ Part_02_PHP_basics/07_chapter_expr_oper_PHP/warning_off_operation/warn.php
@@ -3,6 +3,10 @@
     <input type="submit" name="doGo" value="Click!">
 </form>
 <?php
+    //php8 по умолчанию отключен вывод ошибок
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     // В массиве $_REQUEST всегда содержатся пришедшие из формы данные.
     if ($_REQUEST['doGo']) echo "Вы нажали кнопку!";    
 ?>
