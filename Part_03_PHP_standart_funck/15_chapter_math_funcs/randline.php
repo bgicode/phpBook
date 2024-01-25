b2c5e9295ecd3e4214ea363e76ba385b7350cbdb
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-01-25 12:36:07 +0300

    Sequence of random numbers add mt_srand.php

--- Part_03_PHP_standart_funck/15_chapter_math_funcs/randline.php
+++ Part_03_PHP_standart_funck/15_chapter_math_funcs/randline.php
@@ -1,5 +1,5 @@
 <?php ## Извлечение строки со случайным номером
-    $ourFile = fopen("largetextfile.txt", "r");
+    $ourFile = fopen(__DIR__."/largetextfile.txt", "r");
     // Прочитываем каждую строку файла
     for ($i = 0; $s = fgets($ourFile, 10000); $i++) {
         if (mt_rand(0, $i) == 0) $line = $s;
