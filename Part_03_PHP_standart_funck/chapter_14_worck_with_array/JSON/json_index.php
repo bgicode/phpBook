e31ccd573f9659a6866814434f916781ba9dcbe6
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-01-25 12:20:23 +0300

    extracting string with random number add randlinr.php

--- Part_03_PHP_standart_funck/chapter_14_worck_with_array/JSON/json_index.php
+++ Part_03_PHP_standart_funck/chapter_14_worck_with_array/JSON/json_index.php
@@ -4,11 +4,11 @@
     <title>Передача JSON-данных</title>
     <meta charset='utf-8'>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="/json_index.js"></script>
+    <script src="json_index.js"></script>
 </head>
 <body>
     <p id='js-hello'>Здравствуйте!</p>
-    <form action="/json_answer.php" method="GET">
+    <form action="json_answer.php" method="GET">
         <p>Имя: <input type="text" name="name" value="" /></p>
         <p>Фамилия: <input type="text" name="family" value="" /></p>
         <p><input type="submit" value="Представиться"><p>
