cfb6ae466a5cfeba23fe3566413d0ef2085bb478
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-02-04 19:53:33 +0300

    using microtime() add microtime.php

---
+++ Part_03_PHP_standart_funck/18_chapter_runing_out_programs/microtime.php
@@ -0,0 +1,5 @@
+<?php ## Использование microtime()
+    $time = microtime(true);
+    printf("С начала эпохи UNIX: %f секунд.<br />", $time);
+    echo "С начала эпохи UNIX: $time секунд.<br />";
+?>
