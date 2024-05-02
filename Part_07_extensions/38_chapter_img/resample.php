01a771f49df14d573009a5b44a162f528463d6c4
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-05-02 11:51:45 +0300

    change pen

--- Part_07_extensions/38_chapter_img/resample.php
+++ Part_07_extensions/38_chapter_img/resample.php
@@ -1,21 +1,11 @@
 <?php
-// файл
-$filename = 'sample2.jpg';
-$percent = 0.5;
-
-// тип содержимого
-header('Content-Type: image/jpeg');
-
-// получение новых размеров
-list($width, $height) = getimagesize($filename);
-$new_width = $width * $percent;
-$new_height = $height * $percent;
-
-// ресэмплирование
-$image_p = imagecreatetruecolor($new_width, $new_height);
-$image = imagecreatefromjpeg($filename);
-imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-
-// вывод
-imagejpeg($image_p, null, 100);
+ ## Увеличение картинки со сглаживанием
+$from = imageCreateFromJpeg("button.jpg");
+$to = imageCreateTrueColor(2000, 2000);
+imageCopyResampled(
+    $to, $from, 0, 0, 0, 0, imageSX($to), imageSY($to),
+    imageSX($from), imageSY($from)
+);
+header("Content-type: image/jpeg");
+imageJpeg($to);
 ?>
