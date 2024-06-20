1f45806417a38f251cc17696d878dbdc2db29562
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-06-20 16:17:31 +0300

    get server version

---
+++ Part_07_extensions/37_MySQL/version.php
@@ -0,0 +1,9 @@
+<?php ## Извлечение текущей версии
+    require_once('connect_db.php');
+    // Выполняем запрос
+    $query = "SELECT VERSION() AS version";
+    $ver = $pdo->query($query);
+    // Извлекаем результат
+    $version = $ver->fetch();
+    echo $version['version']; 
+?>
