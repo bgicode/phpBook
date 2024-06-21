9cc52029d0609664db2a22de8ce58ec7c5348179
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-06-21 12:20:47 +0300

    Which servers are selected for each key

--- Part_07_extensions/40_chapter_memcached/set_by_key.php
+++ Part_07_extensions/40_chapter_memcached/set_by_key.php
@@ -1,4 +1,6 @@
 <?php ## Установка значений методом setByKey()
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 require_once("connect_multi.php");
 $arr = ["first", "second", "third", "fourth", "fifth",
         "sixth", "seventh", "eighth", "ninth", "tenth"];
